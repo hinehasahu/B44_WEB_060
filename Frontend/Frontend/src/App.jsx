26c73452919e196b8451272b4b45fd48d2f0c08e
hinehasahu <twinklesahu130@gmail.com>
2025-04-26 10:40:06 +0530

    changes to merge

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -1,64 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/authContext";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import LawyerPanel from "./pages/LawyerPanel";
-import Unauthorized from "./pages/Unauthorized";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
-import CaseDetails from "./pages/CaseDetails";
-import ReportCrime from "./pages/ReportCrime";
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute allowedRoles={["user", "lawyer", "admin"]}>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/lawyer"
-        element={
-          <ProtectedRoute allowedRoles={["lawyer", "admin"]}>
-            <LawyerPanel />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/caseDetails"
-        // element={
-        //   <ProtectedRoute allowedRoles={["lawyer", "admin"]}>
-        //     <CaseDetails />
-        //   </ProtectedRoute>
-        // }
-        element={
-            <CaseDetails />
-        
-        }
-      />
-
-      <Route
-              path="/reportCrime"
-              // element={
-              //   <ProtectedRoute allowedRoles={["lawyer", "admin"]}>
-              //     <CaseDetails />
-              //   </ProtectedRoute>
-              // }
-              element={
-                  <ReportCrime />
-              
-              }
-            />
-      <Route path="/unauthorized" element={<Unauthorized />} />
-      <Route path="*" element={<div>404 Not Found</div>} />
-    </Routes>
+    
+    <>
+    
+    </>
   );
 }
 
